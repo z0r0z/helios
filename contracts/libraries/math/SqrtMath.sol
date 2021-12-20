6a669f4bc8f8e06a4893df12c3f61a59fdaec002
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-20 01:36:13 -0500

    Update SqrtMath.sol
    ⛽ gas optimizations

--- contracts/libraries/math/SqrtMath.sol
+++ contracts/libraries/math/SqrtMath.sol
@@ -2,55 +2,73 @@
 
 pragma solidity >=0.8.0;
 
+/// @notice Babylonian method for computing square roots.
+/// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/FixedPointMathLib.sol)
 library SqrtMath {
-    /// @dev From Hifi.Finance (https://github.com/hifi-finance/prb-math/blob/main/contracts/PRBMath.sol)
     function sqrt(uint256 x) internal pure returns (uint256 result) {
-        if (x == 0) {
-            return 0;
-        }
+        // square root of 0 is zero
+        if (x == 0) return 0;
 
-        // Set the initial guess to the closest power of two that is higher than x.
-        uint256 xAux = uint256(x);
+        // start off with a result of 1
         result = 1;
-        if (xAux >= 0x100000000000000000000000000000000) {
-            xAux >>= 128;
+
+        // used below to help find a nearby power of 2
+        uint256 x2 = x;
+
+        // find the closest power of 2 that is at most x
+        if (x2 >= 0x100000000000000000000000000000000) {
+            x2 >>= 128; // like dividing by 2^128
+
             result <<= 64;
         }
-        if (xAux >= 0x10000000000000000) {
-            xAux >>= 64;
+        if (x2 >= 0x10000000000000000) {
+            x2 >>= 64; // like dividing by 2^64
+
             result <<= 32;
         }
-        if (xAux >= 0x100000000) {
-            xAux >>= 32;
+        if (x2 >= 0x100000000) {
+            x2 >>= 32; // like dividing by 2^32
+
             result <<= 16;
         }
-        if (xAux >= 0x10000) {
-            xAux >>= 16;
+        if (x2 >= 0x10000) {
+            x2 >>= 16; // like dividing by 2^16
+
             result <<= 8;
         }
-        if (xAux >= 0x100) {
-            xAux >>= 8;
+        if (x2 >= 0x100) {
+            x2 >>= 8; // like dividing by 2^8
+
             result <<= 4;
         }
-        if (xAux >= 0x10) {
-            xAux >>= 4;
+        if (x2 >= 0x10) {
+            x2 >>= 4; // like dividing by 2^4
+
             result <<= 2;
         }
-        if (xAux >= 0x8) {
-            result <<= 1;
-        }
+        if (x2 >= 0x8) result <<= 1;
 
-        // The operations can never overflow because the result is max 2^127 when it enters this block.
         unchecked {
+            // shifting right by 1 is like dividing by 2
             result = (result + x / result) >> 1;
+
             result = (result + x / result) >> 1;
+
             result = (result + x / result) >> 1;
+
             result = (result + x / result) >> 1;
+
             result = (result + x / result) >> 1;
+
             result = (result + x / result) >> 1;
-            result = (result + x / result) >> 1; // Seven iterations should be enough
+            
+            result = (result + x / result) >> 1;
+
+            // compute a rounded down version of the result
             uint256 roundedDownResult = x / result;
-            return result >= roundedDownResult ? roundedDownResult : result;
+
+            // if the rounded down result is smaller, use it as the result
+            if (result > roundedDownResult) result = roundedDownResult;
         }
     }
 }
