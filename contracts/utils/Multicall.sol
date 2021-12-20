b120ea76c7c51b19275d443d33629f4b4e43c273
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-20 05:41:52 -0500

    Create Multicall.sol
    ✨ add multicall for tx batching

---
+++ contracts/utils/Multicall.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.8.0;
+
+/// @notice Helper utility that enables calling multiple local methods in a single call.
+/// @author Modified from Uniswap (https://github.com/Uniswap/v3-periphery/blob/main/contracts/base/Multicall.sol)
+abstract contract Multicall {
+    function multicall(bytes[] calldata data) public virtual returns (bytes[] memory results) {
+        results = new bytes[](data.length);
+        
+        // this is reasonably safe from overflow because incrementing `i` loop beyond
+        // 'type(uint256).max' is exceedingly unlikely compared to optimization benefits
+        unchecked {
+            for (uint256 i = 0; i < data.length; i++) {
+                (bool success, bytes memory result) = address(this).delegatecall(data[i]);
+
+                if (!success) {
+                    if (result.length < 68) revert();
+                    
+                    assembly {
+                        result := add(result, 0x04)
+                    }
+                    
+                    revert(abi.decode(result, (string)));
+                }
+                results[i] = result;
+            }
+        }
+    }
+}
