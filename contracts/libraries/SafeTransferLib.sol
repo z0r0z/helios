a4f753c7f1c4db2d93de9389106062141b3ba65e
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-19 20:03:33 -0500

    Create SafeTransferLib.sol
    ⭐ add optimized transfer helper

---
+++ contracts/libraries/SafeTransferLib.sol
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.8.0;
+
+/// @notice Safe ETH and ERC-20 transfer library that gracefully handles missing return values.
+/// @author Modified from SolMate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/SafeTransferLib.sol)
+/// License-Identifier: AGPL-3.0-only
+/// @dev Use with caution! Some functions in this library knowingly create dirty bits at the destination of the free memory pointer.
+library SafeTransferLib {
+    /*///////////////////////////////////////////////////////////////
+                            ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    error ethTransferFailed();
+
+    error transferFailed();
+
+    error transferFromFailed();
+
+    /*///////////////////////////////////////////////////////////////
+                            ETH OPERATIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function _safeTransferETH(address to, uint256 amount) internal {
+        bool callStatus;
+
+        assembly {
+            // transfer the ETH and store if it succeeded or not
+            callStatus := call(gas(), to, amount, 0, 0, 0, 0)
+        }
+
+        if (!callStatus) revert ethTransferFailed();
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                            ERC20 OPERATIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function _safeTransfer(
+        address token,
+        address to,
+        uint256 amount
+    ) internal {
+        bool callStatus;
+
+        assembly {
+            // get a pointer to some free memory
+            let freeMemoryPointer := mload(0x40)
+
+            // write the abi-encoded calldata to memory piece by piece:
+            mstore(freeMemoryPointer, 0xa9059cbb00000000000000000000000000000000000000000000000000000000) // begin with the function selector
+            
+            mstore(add(freeMemoryPointer, 4), and(to, 0xffffffffffffffffffffffffffffffffffffffff)) // mask and append the "to" argument
+            
+            mstore(add(freeMemoryPointer, 36), amount) // finally append the "amount" argument - no mask as it's a full 32 byte value
+
+            // call the token and store if it succeeded or not
+            // we use 68 because the calldata length is 4 + 32 * 2
+            callStatus := call(gas(), token, 0, freeMemoryPointer, 68, 0, 0)
+        }
+
+        if (!_didLastOptionalReturnCallSucceed(callStatus)) revert transferFailed();
+    }
+
+    function _safeTransferFrom(
+        address token,
+        address from,
+        address to,
+        uint256 amount
+    ) internal {
+        bool callStatus;
+
+        assembly {
+            // get a pointer to some free memory
+            let freeMemoryPointer := mload(0x40)
+
+            // write the abi-encoded calldata to memory piece by piece:
+            mstore(freeMemoryPointer, 0x23b872dd00000000000000000000000000000000000000000000000000000000) // begin with the function selector
+            
+            mstore(add(freeMemoryPointer, 4), and(from, 0xffffffffffffffffffffffffffffffffffffffff)) // mask and append the "from" argument
+            
+            mstore(add(freeMemoryPointer, 36), and(to, 0xffffffffffffffffffffffffffffffffffffffff)) // mask and append the "to" argument
+            
+            mstore(add(freeMemoryPointer, 68), amount) // finally append the "amount" argument - no mask as it's a full 32 byte value
+
+            // call the token and store if it succeeded or not
+            // we use 100 because the calldata length is 4 + 32 * 3
+            callStatus := call(gas(), token, 0, freeMemoryPointer, 100, 0, 0)
+        }
+
+        if (!_didLastOptionalReturnCallSucceed(callStatus)) revert transferFromFailed();
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                            INTERNAL HELPER LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function _didLastOptionalReturnCallSucceed(bool callStatus) internal pure returns (bool success) {
+        assembly {
+            // get how many bytes the call returned
+            let returnDataSize := returndatasize()
+
+            // if the call reverted:
+            if iszero(callStatus) {
+                // copy the revert message into memory
+                returndatacopy(0, 0, returnDataSize)
+
+                // revert with the same message
+                revert(0, returnDataSize)
+            }
+
+            switch returnDataSize
+            
+            case 32 {
+                // copy the return data into memory
+                returndatacopy(0, 0, returnDataSize)
+
+                // set success to whether it returned true
+                success := iszero(iszero(mload(0)))
+            }
+            case 0 {
+                // there was no return data
+                success := 1
+            }
+            default {
+                // it returned some malformed input
+                success := 0
+            }
+        }
+    }
+}
