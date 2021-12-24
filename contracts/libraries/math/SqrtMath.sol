8bf84ce42fb5ff5d2b21586ef55984abcd95086a
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-24 15:27:56 -0500

    Update SqrtMath.sol
    ⛽ gas optimizations via assembly

--- contracts/libraries/math/SqrtMath.sol
+++ contracts/libraries/math/SqrtMath.sol
@@ -6,69 +6,61 @@ pragma solidity >=0.8.4;
 /// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/FixedPointMathLib.sol)
 library SqrtMath {
     function sqrt(uint256 x) internal pure returns (uint256 result) {
-        // square root of 0 is zero
-        if (x == 0) return 0;
+        assembly {
+            // if x is zero, just return 0
+            if iszero(iszero(x)) {
+                // start off with a result of 1
+                result := 1
 
-        // start off with a result of 1
-        result = 1;
+                // used below to help find a nearby power of 2
+                let x2 := x
 
-        // used below to help find a nearby power of 2
-        uint256 x2 = x;
+                // find the closest power of 2 that is at most x
+                if iszero(lt(x2, 0x100000000000000000000000000000000)) {
+                    x2 := shr(128, x2) // like dividing by 2^128
+                    result := shl(64, result)
+                }
+                if iszero(lt(x2, 0x10000000000000000)) {
+                    x2 := shr(64, x2) // like dividing by 2^64
+                    result := shl(32, result)
+                }
+                if iszero(lt(x2, 0x100000000)) {
+                    x2 := shr(32, x2) // like dividing by 2^32
+                    result := shl(16, result)
+                }
+                if iszero(lt(x2, 0x10000)) {
+                    x2 := shr(16, x2) // like dividing by 2^16
+                    result := shl(8, result)
+                }
+                if iszero(lt(x2, 0x100)) {
+                    x2 := shr(8, x2) // like dividing by 2^8
+                    result := shl(4, result)
+                }
+                if iszero(lt(x2, 0x10)) {
+                    x2 := shr(4, x2) // like dividing by 2^4
+                    result := shl(2, result)
+                }
+                if iszero(lt(x2, 0x8)) {
+                    result := shl(1, result)
+                }
 
-        // find the closest power of 2 that is at most x
-        if (x2 >= 0x100000000000000000000000000000000) {
-            x2 >>= 128; // like dividing by 2^128
+                // shifting right by 1 is like dividing by 2
+                result := shr(1, add(result, div(x, result)))
+                result := shr(1, add(result, div(x, result)))
+                result := shr(1, add(result, div(x, result)))
+                result := shr(1, add(result, div(x, result)))
+                result := shr(1, add(result, div(x, result)))
+                result := shr(1, add(result, div(x, result)))
+                result := shr(1, add(result, div(x, result)))
 
-            result <<= 64;
-        }
-        if (x2 >= 0x10000000000000000) {
-            x2 >>= 64; // like dividing by 2^64
+                // compute a rounded down version of the result
+                let roundedDownResult := div(x, result)
 
-            result <<= 32;
-        }
-        if (x2 >= 0x100000000) {
-            x2 >>= 32; // like dividing by 2^32
-
-            result <<= 16;
-        }
-        if (x2 >= 0x10000) {
-            x2 >>= 16; // like dividing by 2^16
-
-            result <<= 8;
-        }
-        if (x2 >= 0x100) {
-            x2 >>= 8; // like dividing by 2^8
-
-            result <<= 4;
-        }
-        if (x2 >= 0x10) {
-            x2 >>= 4; // like dividing by 2^4
-
-            result <<= 2;
-        }
-        if (x2 >= 0x8) result <<= 1;
-
-        unchecked {
-            // shifting right by 1 is like dividing by 2
-            result = (result + x / result) >> 1;
-
-            result = (result + x / result) >> 1;
-
-            result = (result + x / result) >> 1;
-
-            result = (result + x / result) >> 1;
-
-            result = (result + x / result) >> 1;
-
-            result = (result + x / result) >> 1;
-            
-            result = (result + x / result) >> 1;
-
-            // compute a rounded down version of the result
-            uint256 roundedDownResult = x / result;
-
-            // if the rounded down result is smaller, use it as the result
-            if (result > roundedDownResult) result = roundedDownResult;
+                // if the rounded down result is smaller, use it
+                if gt(result, roundedDownResult) {
+                    result := roundedDownResult
+                }
+            }
         }
     }
 }
