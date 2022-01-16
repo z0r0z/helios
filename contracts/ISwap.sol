e7c691aaffeeab369c464964e648181c97786884
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-01-16 04:51:49 -0500

    Update ISwap.sol
    🥢 add id params

--- contracts/ISwap.sol
+++ contracts/ISwap.sol
@@ -2,10 +2,11 @@
 
 pragma solidity >=0.8.4;
 
+/// @notice Helios interface for liquidity management and swap.
 interface ISwap {
-    function addLiquidity(uint256 token0amount, uint256 token1amount) external returns (uint256 lp);
+    function addLiquidity(uint256 id, uint256 token0amount, uint256 token1amount) external returns (uint256 lp);
 
-    function removeLiquidity(uint256 lp) external returns (uint256 amount0out, uint256 amount1out);
+    function removeLiquidity(uint256 id, uint256 lp) external returns (uint256 amount0out, uint256 amount1out);
 
-    function swap(address tokenIn, uint256 amountIn) external returns (uint256 amountOut);
+    function swap(uint256 id, address tokenIn, uint256 amountIn) external returns (uint256 amountOut);
 }
