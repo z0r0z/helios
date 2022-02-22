4213a9807771d1515ce2666e1b68285308439618
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-22 14:50:32 -0700

    Update ReentrancyGuard.sol
    style

--- contracts/utils/ReentrancyGuard.sol
+++ contracts/utils/ReentrancyGuard.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.8.4;
 
-/// @notice Gas optimized reentrancy protection for smart contracts.
+/// @notice Gas optimized reentrancy protection for smart contracts
 /// @author Modified from Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/ReentrancyGuard.sol)
 abstract contract ReentrancyGuard {
     uint256 private locked = 1;
