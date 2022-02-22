77597ab38f39afacfeea0e8a3d6fbf265e358c03
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-22 14:54:42 -0700

    Update and rename contracts/ISwap.sol to contracts/interfaces/IPairSwap.sol
    reformat

---
+++ contracts/interfaces/IPairSwap.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.8.4;
+
+/// @notice Helios interface for liquidity management and pair swap
+interface IPairSwap {
+    function addLiquidity(uint256 id, uint256 token0amount, uint256 token1amount) external returns (uint256 liq);
+
+    function removeLiquidity(uint256 id, uint256 liq) external returns (uint256 amount0out, uint256 amount1out);
+
+    function swap(uint256 id, address tokenIn, uint256 amountIn) external returns (uint256 amountOut);
+}
