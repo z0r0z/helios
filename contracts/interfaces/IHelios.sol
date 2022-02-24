e47573d2a58bd9fa8ab4af1ebc879d878f1bafd1
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-23 18:08:36 -0700

    Create IHelios.sol
    add helios swap interface

---
+++ contracts/interfaces/IHelios.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.8.4;
+
+/// @notice Interface for Helios swap
+interface IHelios {
+    struct Pair {
+        address token0; 
+        address token1; 
+        address swapper; 
+        uint112 reserve0; 
+        uint112 reserve1; 
+        uint8 fee;
+    }
+
+    function pairs(uint256 id) external view returns (Pair memory pair);
+
+    function totalSupplyForId(uint256 id) external view returns (uint256);
+}
