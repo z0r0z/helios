9ab32a15e6d9fecf92488de886fa17ae8b9c90f7
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-18 20:00:57 -0400

    Update Multicall.sol
    resolve https://github.com/z0r0z/Helios/issues/13

--- contracts/utils/Multicall.sol
+++ contracts/utils/Multicall.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-
 pragma solidity >=0.8.4;
 
 /// @notice Helper utility that enables calling multiple local methods in a single call
 /// @author Modified from Uniswap (https://github.com/Uniswap/v3-periphery/blob/main/contracts/base/Multicall.sol)
 /// License-Identifier: GPL-2.0-or-later
 abstract contract Multicall {
-    function multicall(bytes[] calldata data) public payable returns (bytes[] memory results) {
+    function multicall(bytes[] calldata data) external returns (bytes[] memory results) {
         results = new bytes[](data.length);
         
         for (uint256 i; i < data.length;) {
