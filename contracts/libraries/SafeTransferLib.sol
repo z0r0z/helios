9d1188db8896795ce9116c2ffe0b26c6c9c37a95
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-18 20:11:01 -0400

    Update SafeTransferLib.sol
    🧹bring up to speed with latest solmate

--- contracts/libraries/SafeTransferLib.sol
+++ contracts/libraries/SafeTransferLib.sol
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-
 pragma solidity >=0.8.4;
 
 /// @notice Safe ETH and ERC-20 transfer library that gracefully handles missing return values
 /// @author Modified from Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/SafeTransferLib.sol)
 /// License-Identifier: AGPL-3.0-only
-/// @dev Use with caution! Some functions in this library knowingly create dirty bits at the destination of the free memory pointer
-/// @dev Note that none of the functions in this library check that a token has code at all! That responsibility is delegated to the caller
 library SafeTransferLib {
     /// -----------------------------------------------------------------------
     /// Errors
@@ -45,26 +42,21 @@ library SafeTransferLib {
         assembly {
             // get a pointer to some free memory
             let freeMemoryPointer := mload(0x40)
-
             // write the abi-encoded calldata into memory, beginning with the function selector
             mstore(freeMemoryPointer, 0xa9059cbb00000000000000000000000000000000000000000000000000000000)
-            mstore(add(freeMemoryPointer, 4), to) // mask and append the "to" argument
-            mstore(add(freeMemoryPointer, 36), amount) // append the "amount" argument
+            mstore(add(freeMemoryPointer, 4), to) // append the 'to' argument
+            mstore(add(freeMemoryPointer, 36), amount) // append the 'amount' argument
 
-            // fill up the scratch space so it's easy to tell if the call returns <32 bytes
-            mstore(0, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF)
-
-            // call the token and store if it succeeded or not
-            // we use 68 because the calldata length is 4 + 32 * 2
-            // we'll copy up to 32 bytes of return data into the scratch space,
-            // if it returns <32 bytes at least a portion of the junk will remain
-            success := call(gas(), token, 0, freeMemoryPointer, 68, 0, 32)
-
-            // set success to whether the call returned 1, except if it
-            // had no return data, in which case we assume it succeeded,
-            // or if it reverted, in which case we multiply everything by
-            // 0, setting success to zero which will decode as false below
-            success := mul(add(iszero(returndatasize()), eq(mload(0), 1)), success)
+            success := and(
+                // set success to whether the call reverted, if not we check it either
+                // returned exactly 1 (not just any non-zero data), or had no return data
+                or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
+                // we use 68 because the length of our calldata totals up like so: 4 + 32 * 2
+                // we use 0 and 32 to copy up to 32 bytes of return data into the scratch space
+                // counterintuitively, this call must be positioned second to the addition in the
+                // order of operations or else returndatasize() will be zero during the computation
+                call(gas(), token, 0, freeMemoryPointer, 68, 0, 32)
+            )
         }
 
         if (!success) revert TransferFailed();
@@ -81,27 +73,22 @@ library SafeTransferLib {
         assembly {
             // get a pointer to some free memory
             let freeMemoryPointer := mload(0x40)
-
             // write the abi-encoded calldata into memory, beginning with the function selector
             mstore(freeMemoryPointer, 0x23b872dd00000000000000000000000000000000000000000000000000000000)
-            mstore(add(freeMemoryPointer, 4), from) // mask and append the "from" argument
-            mstore(add(freeMemoryPointer, 36), to) // mask and append the "to" argument
-            mstore(add(freeMemoryPointer, 68), amount) // append the "amount" argument
+            mstore(add(freeMemoryPointer, 4), from) // append the 'from' argument
+            mstore(add(freeMemoryPointer, 36), to) // append the 'to' argument
+            mstore(add(freeMemoryPointer, 68), amount) // append the 'amount' argument
 
-            // fill up the scratch space so it's easy to tell if the call returns <32 bytes
-            mstore(0, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF)
-
-            // call the token and store if it succeeded or not
-            // we use 100 because the calldata length is 4 + 32 * 3
-            // we'll copy up to 32 bytes of return data into the scratch space,
-            // if it returns <32 bytes at least a portion of the junk will remain
-            success := call(gas(), token, 0, freeMemoryPointer, 100, 0, 32)
-
-            // set success to whether the call returned 1, except if it
-            // had no return data, in which case we assume it succeeded,
-            // or if it reverted, in which case we multiply everything by
-            // 0, setting success to zero which will decode as false below
-            success := mul(add(iszero(returndatasize()), eq(mload(0), 1)), success)
+            success := and(
+                // set success to whether the call reverted, if not we check it either
+                // returned exactly 1 (not just any non-zero data), or had no return data
+                or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
+                // we use 100 because the length of our calldata totals up like so: 4 + 32 * 3
+                // we use 0 and 32 to copy up to 32 bytes of return data into the scratch space
+                // counterintuitively, this call must be positioned second to the addition in the
+                // order of operations or else returndatasize() will be zero during the computation
+                call(gas(), token, 0, freeMemoryPointer, 100, 0, 32)
+            )
         }
 
         if (!success) revert TransferFromFailed();
