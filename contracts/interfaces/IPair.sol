fb0653b962f3a73d0c1eb974765225f82a3ab224
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-24 21:08:01 -0400

    Update IPair.sol
    spacing

--- contracts/interfaces/IPair.sol
+++ contracts/interfaces/IPair.sol
@@ -4,6 +4,8 @@ pragma solidity >=0.8.4;
 /// @notice Helios interface for liquidity management and pair swap
 interface IPair {
     function addLiquidity(uint256 id, uint256 token0amount, uint256 token1amount) external returns (uint256 liq);
+
     function removeLiquidity(uint256 id, uint256 liq) external returns (uint256 amount0out, uint256 amount1out);
+    
     function swap(uint256 id, address tokenIn, uint256 amountIn) external returns (uint256 amountOut);
 }
